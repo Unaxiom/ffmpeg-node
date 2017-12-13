a37d90c69b909d9b92fd16e14e7b7b40cd47e2c2
Pratheek Adidela <apratheek@unaxiom.com>
2017-12-13 16:15:43 +0530

    Reintroduced API to set the output file name. Works now.

--- ffmpeg.ts
+++ ffmpeg.ts
@@ -9,8 +9,10 @@ import child_process = require('child_process');
 export class FFmpeg {
     private options: string[]
     private runProcess: child_process.ChildProcess
+    private outputFilename: string
     constructor() {
         this.options = [];
+        this.outputFilename = "";
     }
     /**Adds a CLI option to the list */
     addOptions(optionsList: string[]): void {
@@ -22,27 +24,38 @@ export class FFmpeg {
     addOption(option: string): void {
         this.options.push(option);
     }
+    /**Sets the output file name */
+    setOutputFile(filename: string): void {
+        this.outputFilename = filename;
+    }
     /**Returns the arguments */
     private returnCLIArgs(): string[] {
+        if (this.outputFilename != "") {
+            let temp = this.options;
+            temp.push(this.outputFilename);
+            return temp;
+        }
         return this.options;
     }
     /**Begins the FFmpeg process */
     run(): void {
         // Run the command here
         this.runProcess = child_process.spawn('ffmpeg', this.returnCLIArgs());
+        this.runProcess.stdin.setDefaultEncoding('utf-8');
         this.runProcess.stderr.pipe(process.stderr);
-
-        this.runProcess.on('exit', function (code: number, signal: string) {
-            console.log("Process has closed with code -> " + code + " and signal -> " + signal);
-        });
     }
     /**Quits the FFmpeg process */
-    quit(): void {
+    quit(callbackFunc?: Function): void {
+        if (callbackFunc !== undefined && callbackFunc !== null) {
+            this.runProcess.on('exit', function (code, signal) {
+                callbackFunc(code, signal);
+            });
+        }
+
         // Send the `q` key
-        this.runProcess.stdin.setDefaultEncoding('utf-8');
-        this.runProcess.stdin.write('q');
-        // console.log("Writing q to stdin");
-        // this.runProcess.kill('SIGINT')
+        if (!this.runProcess.killed) {
+            this.runProcess.stdin.write('q');
+        }
     }
     /**Kills the process forcefully (might not save the output) */
     kill(): void {
@@ -61,11 +74,36 @@ function main() {
         "-vf", "setpts=80*PTS",
         "output.webm"
     ]);
+    ffmpeg.setOutputFile("output.webm");
+    // ffmpeg.addOptions([
+    //     "-y",
+    //     "-r", "240",
+    //     "-f", "x11grab",
+    //     "-draw_mouse", "0",
+    //     "-s", "1920x1080",
+    //     "-i", ":99",
+    //     "-c:v", "libvpx",
+    //     "-b:v", "384k",
+    //     "-qmin", "7",
+    //     "-qmax", "25",
+    //     "-maxrate", "384k",
+    //     "-bufsize", "1000k",
+    //     "-metadata", "title=Selenium Recording",
+    //     "-metadata", "artist=Unaxiom Technologies",
+    //     "-metadata", "album=Selenium Recordings",
+    //     "-metadata", `year=${new Date().getFullYear().toString()}`,
+    //     // "screen.vb8.webm"
+    // ]);
+    // ffmpeg.setOutputFile("screen.vb8.webm");
     ffmpeg.run();
     setTimeout(function () {
-        console.log("++++++++++++++++++++++++++\nAbout to quit\n\n\n");
-        ffmpeg.quit();
-        console.log("+++++++++++++++++\nSuccessfully quit\n");
+        try {
+            ffmpeg.quit(function (code: number, signal: string) {
+                console.log("Process has been quit from callback with code -> " + code + " and signal -> " + signal);
+            });
+        } catch (e) {
+            throw (e);
+        }
     }, 5000);
 }
 
--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -9,6 +9,7 @@ var child_process = require("child_process");
 var FFmpeg = /** @class */ (function () {
     function FFmpeg() {
         this.options = [];
+        this.outputFilename = "";
     }
     /**Adds a CLI option to the list */
     FFmpeg.prototype.addOptions = function (optionsList) {
@@ -21,26 +22,37 @@ var FFmpeg = /** @class */ (function () {
     FFmpeg.prototype.addOption = function (option) {
         this.options.push(option);
     };
+    /**Sets the output file name */
+    FFmpeg.prototype.setOutputFile = function (filename) {
+        this.outputFilename = filename;
+    };
     /**Returns the arguments */
     FFmpeg.prototype.returnCLIArgs = function () {
+        if (this.outputFilename != "") {
+            var temp = this.options;
+            temp.push(this.outputFilename);
+            return temp;
+        }
         return this.options;
     };
     /**Begins the FFmpeg process */
     FFmpeg.prototype.run = function () {
         // Run the command here
         this.runProcess = child_process.spawn('ffmpeg', this.returnCLIArgs());
+        this.runProcess.stdin.setDefaultEncoding('utf-8');
         this.runProcess.stderr.pipe(process.stderr);
-        this.runProcess.on('exit', function (code, signal) {
-            console.log("Process has closed with code -> " + code + " and signal -> " + signal);
-        });
     };
     /**Quits the FFmpeg process */
-    FFmpeg.prototype.quit = function () {
+    FFmpeg.prototype.quit = function (callbackFunc) {
+        if (callbackFunc !== undefined && callbackFunc !== null) {
+            this.runProcess.on('exit', function (code, signal) {
+                callbackFunc(code, signal);
+            });
+        }
         // Send the `q` key
-        this.runProcess.stdin.setDefaultEncoding('utf-8');
-        this.runProcess.stdin.write('q');
-        // console.log("Writing q to stdin");
-        // this.runProcess.kill('SIGINT')
+        if (!this.runProcess.killed) {
+            this.runProcess.stdin.write('q');
+        }
     };
     /**Kills the process forcefully (might not save the output) */
     FFmpeg.prototype.kill = function () {
@@ -60,11 +72,37 @@ function main() {
         "-vf", "setpts=80*PTS",
         "output.webm"
     ]);
+    ffmpeg.setOutputFile("output.webm");
+    // ffmpeg.addOptions([
+    //     "-y",
+    //     "-r", "240",
+    //     "-f", "x11grab",
+    //     "-draw_mouse", "0",
+    //     "-s", "1920x1080",
+    //     "-i", ":99",
+    //     "-c:v", "libvpx",
+    //     "-b:v", "384k",
+    //     "-qmin", "7",
+    //     "-qmax", "25",
+    //     "-maxrate", "384k",
+    //     "-bufsize", "1000k",
+    //     "-metadata", "title=Selenium Recording",
+    //     "-metadata", "artist=Unaxiom Technologies",
+    //     "-metadata", "album=Selenium Recordings",
+    //     "-metadata", `year=${new Date().getFullYear().toString()}`,
+    //     // "screen.vb8.webm"
+    // ]);
+    // ffmpeg.setOutputFile("screen.vb8.webm");
     ffmpeg.run();
     setTimeout(function () {
-        console.log("++++++++++++++++++++++++++\nAbout to quit\n\n\n");
-        ffmpeg.quit();
-        console.log("+++++++++++++++++\nSuccessfully quit\n");
+        try {
+            ffmpeg.quit(function (code, signal) {
+                console.log("Process has been quit from callback with code -> " + code + " and signal -> " + signal);
+            });
+        }
+        catch (e) {
+            throw (e);
+        }
     }, 5000);
 }
 main();
