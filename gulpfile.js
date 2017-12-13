95278ba556c87a2650b5ddbb5ea54351614a5b61
Pratheek Adidela <apratheek@unaxiom.com>
2017-12-13 16:15:15 +0530

    Removed redundant gulp packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 var gulp = require('gulp');
-// var browserify = require('browserify');
 var tsb = require('gulp-tsb');
-// var watchify = require('watchify');
-// var assign = require('lodash.assign');
-// var gutil = require('gulp-util');
-// var uglify = require('gulp-uglify');
 var notifier = require('node-notifier');
-// var source = require('vinyl-source-stream');
-// var buffer = require('vinyl-buffer');
-// var sourcemaps = require('gulp-sourcemaps');
 
 // create and keep compiler
 var compilation = tsb.create({
@@ -17,7 +9,7 @@ var compilation = tsb.create({
     declaration: false,
     moduleResolution: 'node',
     types: ["node"],
-    lib: ["es2015", "es2015.promise", "dom", "es5"],
+    lib: ["es2015", "es2015.promise", "es5"],
 });
 
 // Set up src ts build task
